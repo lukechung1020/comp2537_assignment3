60388a6c0dd71edad6426463552a1f58a6715a31
Luke Chung <lukechung1020@gmail.com>
2023-05-15 15:21:24 -0700

    Added modal that displays more pokemon information

--- pokemon.js
+++ pokemon.js
@@ -14,8 +14,72 @@ const setup = async () => {
       <div class="pokeCard card" pokeName=${res.data.name}   >
         <h3>${res.data.name.toUpperCase()}</h3> 
         <img src="${res.data.sprites.front_default}" alt="${res.data.name}"/>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#pokeModal">
+          More
+        </button>
       </div>  
     `);
+  });
+
+  // pop up modal when clicking on a pokemon card
+  // add event listener to each pokemon card
+  $("body").on("click", ".pokeCard", async function (e) {
+    const pokemonName = $(this).attr("pokeName");
+    // console.log("pokemonName: ", pokemonName);
+    const res = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    // console.log("res.data: ", res.data);
+    const types = res.data.types.map((type) => type.type.name);
+    $(".modal-body").html(`
+        <div style="width:200px">
+        <img src="${
+          res.data.sprites.other["official-artwork"].front_default
+        }" alt="${res.data.name}"/>
+        <div>
+        <h3>Abilities</h3>
+        <ul>
+        ${res.data.abilities
+          .map((ability) => `<li>${ability.ability.name}</li>`)
+          .join("")}
+        </ul>
+        </div>
+
+        <div>
+        <h3>Stats</h3>
+        <ul>
+        ${res.data.stats
+          .map((stat) => `<li>${stat.stat.name}: ${stat.base_stat}</li>`)
+          .join("")}
+        </ul>
+
+        </div>
+
+        </div>
+          <h3>Types</h3>
+          <ul>
+          ${types
+            .map(
+              (type) =>
+                `<li>${type.charAt(0).toUpperCase() + type.slice(1)}</li>`
+            )
+            .join("")}
+          </ul>
+      
+        `);
+    $(".modal-title").html(`
+        <h2>${res.data.name.toUpperCase()}</h2>
+        <h5>${res.data.id}</h5>
+        `);
+  });
+
+  // add event listener to pagination buttons
+  $("body").on("click", ".numberedButtons", async function (e) {
+    currentPage = Number(e.target.value);
+    paginate(currentPage, PAGE_SIZE, pokemon);
+
+    //update pagination buttons
+    updatePaginationDiv(currentPage, numPages);
   });
 };
 
